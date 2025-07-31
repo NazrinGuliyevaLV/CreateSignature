2655a42e340593f459f3934e1cf02e0e07c86a23
NazrinGuliyevaLV <Nazrin.Guliyeva@lv-logistics.com>
2025-07-31 15:12:11 +0400

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000; 
+const port = 3001; 
 
 app.use(bodyParser.json())
 app.use(express.json());
